ad2a424ac103038cc3a353b1bca4c58bdba17611
mulandi <mulandimorris1@gmail.com>
2025-05-27 22:32:14 +0300

    thunder client setup

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
-const app = express();
+const port = process.env.PORT || 5000;
 
-const port = 5000;
+app = express();
+
+
+app.get("/msm", (req, res) => {
+ res.json({ message: "get all contacts" })
+})
+
+app.listen(port, () => {
+ console.log(`Server running on port ${port}`);
+})
