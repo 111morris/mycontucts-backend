961eca43132b96a090d97cc29d8dde5ffc0ae706
mulandi <mulandimorris1@gmail.com>
2025-05-27 22:43:06 +0300

    dvpkg

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 
-router.route("/msm", (req, res) => {
- res.json({ message: "get all contacts" });
+router.route("/").get((req, res) => {
+ // res.json({ message: "get all contacts" });
  res.status(200).json({ message: "getting all contacts" });
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const port = process.env.PORT || 5000;
 
 app = express();
 
-
-
-//app.get()
+app.use("/msm", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
  console.log(`Server running on port ${port}`);
