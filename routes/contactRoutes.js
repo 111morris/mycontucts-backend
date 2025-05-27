462d8c56acda419dedfe4abe55f2811d03960efd
mulandi <mulandimorris1@gmail.com>
2025-05-27 23:02:29 +0300

    dvpkg

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,10 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
+
+//get
+//post
+//delete
+//put (update)
 router.route("/").get((req, res) => {
- res.status(200).json({ message: "hello morris how have you been of late" });
+ res.status(200).json({ message: "get all contacts" });
+ 
 });
 
+router.route("/").post((req, res) => {
+ res.status(200).json({ message: "create contact" });
+})
 
+router.route("/:id").put((req, res) => {
+ res.status(200).json({ message: `Update contact for ${req.params.id}` });
+})
 
+router.route("/:id").get((req, res) => {
+ res.status(200).json({message: `Delete contact for ${req.params.id}`}) 
+})
 module.exports = router;
