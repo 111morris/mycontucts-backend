1a386be4ffb4ea89e200af66a959d9783c44c9dc
mulandi <mulandimorris1@gmail.com>
2025-05-28 12:54:43 +0300

    express throw error

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -18,6 +18,12 @@ const getContact = (req, res) => {
 //@access public
 const createContact = (req, res) => {
  console.log("The request body is: ", req.body);
+ const { name, email, phone } = req.body;
+ 
+ if (!name || !email || !phone) {
+  res.status(400);
+  throw new Error("please enter all the fields");
+ }
  res.status(201).json({ message: "create contact" });
 }
 
