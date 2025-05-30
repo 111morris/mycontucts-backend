0f77b4fdd93935f1fe8e3aed00f8de8403e7b3cd
mulandi <mulandimorris1@gmail.com>
2025-05-30 07:29:30 +0300

    adding user routes - registration, login and current

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+ res.status(200).json({ message: "Register the user" });
+})
+
+router.post("/login", (req, res) => {
+ res.status(200).json({ message: "login user" });
+})
+
+router.get("/current", (req, res) => {
+ res.status(200).json({message: "current user information"})
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const port = process.env.PORT || 5000;
 
 
 app.use(express.json());
-app.use("/msm", require("./routes/contactRoutes"));
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler)
+
 
 app.listen(port, () => {
  console.log(`Server is running on port ${port}`); 
